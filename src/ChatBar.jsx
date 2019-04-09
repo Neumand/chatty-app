3f1799e3ac9dbd0c2974df476013a80ddced4f0b
David <david.m.neuman@gmail.com>
2019-04-09 17:39:51 +0000

    Added event listener for a user submitting a new message.

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,15 +1,24 @@
 import React, { Component } from "react";
 
 const ChatBar = ({ currentUser }) => {
+  const newMessage = e => {
+    if (e.key === "Enter") {
+      const messageContent = e.target.value;
+      addNewMessage(messageContent);
+      e.target.value = "";
+    }
+  };
   return (
-      <footer className="chatbar">
-        <input className="chatbar-username" defaultValue={currentUser} />
-        <input
-          className="chatbar-message"
-          placeholder="Type a message and hit ENTER"
-        />
-      </footer>
+    <footer className="chatbar">
+      <input className="chatbar-username" defaultValue={currentUser} />
+      <input
+        type="text"
+        className="chatbar-message"
+        placeholder="Type a message and hit ENTER"
+        onKeyDown={newMessage}
+      />
+    </footer>
   );
-}
+};
 
 export default ChatBar;
