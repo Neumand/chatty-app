042064b2e023e070111f08b93456e2fb98860ec0
David <david.m.neuman@gmail.com>
2019-04-09 16:01:57 +0000

    Created template state. Passed messages down to MessageList.

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,26 @@ import ChatBar from "./ChatBar.jsx";
 class App extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      currentUser: { name: "Bob" },
+      messages: [
+        {
+          username: "Bob",
+          content: "Has anyone seen my marbles?"
+        },
+        {
+          username: "Anonymous",
+          content:
+            "No, I think you lost them. You lost your marbles, Bob. You lost them for good."
+        }
+      ]
+    };
   }
   render() {
     return (
       <div>
         <Navbar />
-        <MessageList />
+        <MessageList messages={this.state.messages} />
         <ChatBar />
       </div>
     );
