3179fe96cdf413d4a9ba40402c144cb7963f1401
David <david.m.neuman@gmail.com>
2019-04-10 22:03:48 +0000

    Added conditional to display 'user' if only one user online.

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
 const Navbar = ({ userCount }) => {
+  let user = '';
+  userCount === 1 ? user = 'user' : user = 'users';
   return (
     <nav className='navbar'>
       <a href='/' className='navbar-brand'>
         Chatty
       </a>
-      <p className='navbar-online-users'>{`${userCount} online users`}</p>
+      <p className='navbar-online-users'>{`${userCount} online ${user}`}</p>
     </nav>
   );
 };
