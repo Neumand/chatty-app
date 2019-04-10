1b59425c0d71212a88992f17e3a4c2aa6069e71a
David <david.m.neuman@gmail.com>
2019-04-10 20:39:55 +0000

    Added conditional statement to handle different data types and render different content.

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -1,12 +1,24 @@
-import React, { Component } from "react";
-import Message from "./Message.jsx";
+import React, { Component } from 'react';
+import Message from './Message.jsx';
+import Notification from './Notification.jsx';
 
 const MessageList = ({ messages }) => {
-  const messageList = messages.map(message =>
-    <Message key={message.id} content={message.content} username={message.username} />);
-  return (
-      <main>{messageList}</main>
-  );
+  const messageList = messages.map(message => {
+    if (message.type === 'incomingMessage') {
+      return (
+      <Message
+        key={message.id}
+        content={message.content}
+        username={message.username}
+      />)
+    } else {
+      return (
+      <Notification
+        notification={message.content}
+      />)
+    }
+  });
+  return <main>{messageList}</main>;
 };
 
 export default MessageList;
