e4273016548b39516ba880cb73c077fa5df5f7dd
David <david.m.neuman@gmail.com>
2019-04-10 20:38:53 +0000

    Small edit.

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,16 +1,13 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 const Message = ({ username, content }) => {
   return (
     <div>
-      <div className="message">
-        <span className="message-username">{username}</span>
-        <span className="message-content">
-          {content}
-        </span>
+      <div className='message'>
+        <span className='message-username'>{username}</span>
+        <span className='message-content'>{content}</span>
       </div>
-      <div className="message system">
-      </div>
+      <div className='message system' />
     </div>
   );
 };
