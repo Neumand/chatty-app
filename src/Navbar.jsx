eca02a61997f9aabb18125e1d84f5646e55afcf3
David <david.m.neuman@gmail.com>
2019-04-11 15:39:59 +0000

    Added UFO image to the navbar and styled it.

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 
 const Navbar = ({ userCount }) => {
   let user = '';
-  userCount === 1 ? user = 'user' : user = 'users';
+  userCount === 1 ? (user = 'user') : (user = 'users');
   return (
     <nav className='navbar'>
       <a href='/' className='navbar-brand'>
         Chatty
       </a>
+      <img
+        className="navbar-brand"
+        src='https://cdn2.iconfinder.com/data/icons/icontober/64/Inkcontober_Misterious-512.png'
+        alt='UFO'
+      />
       <p className='navbar-online-users'>{`${userCount} online ${user}`}</p>
     </nav>
   );
