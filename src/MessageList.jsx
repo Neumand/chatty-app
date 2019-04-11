e569f7ac9441af8249f3eba2d90cf26050dc4665
David <david.m.neuman@gmail.com>
2019-04-11 16:55:33 +0000

    Added message display if the type is a GIF.

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -13,13 +13,32 @@ const MessageList = ({ messages }) => {
           emoji={message.emoji}
         />
       );
+    } else if (message.type === 'incomingGIF') {
+      return (
+        <Message
+          key={message.id}
+          content={
+            <div>
+              <div>{message.content.title}</div>
+              <div>
+                <img
+                  src={message.content.imageUrl}
+                  alt={message.content.title}
+                />
+              </div>
+            </div>
+          }
+          username={message.username}
+          emoji={message.emoji}
+        />
+      );
     } else {
       return <Notification key={message.id} notification={message.content} />;
     }
   });
   return (
     <main>
-      <h1 className="message-username">Welcome to the Chatty Community!</h1>
+      <h1 className='message-username'>Welcome to the Chatty Community!</h1>
       {messageList}
     </main>
   );
